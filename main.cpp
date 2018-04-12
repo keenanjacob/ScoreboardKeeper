852d4d4de4a4a4868987808549ece2cea2d98bb8
keenanjacob <kjacob1@mail.csuchico.edu>
2018-04-11 18:29:42 -0700

    first with function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -8,8 +10,12 @@ const int MAX_TEAMS = 4;
 const int MIN_PERIODS = 1;
 const int MIN_TEAMS = 1;
 
+void printScoreboard(vector<vector<int> > );
+int randomBetween(int, int);
+
 int main()
 {
+  srand((int) time(0));
   int periods;
   int teams;
   vector<vector<int> > board;
@@ -28,26 +34,52 @@ int main()
   }
   else
   {
-   //make scoreboard and fill it with zeros
-   board.resize(teams);
-   for (int row=0; row<board.size(); row++)
-   {
-     board[row].resize(periods);
-   }
-   cout<< "SCOREBOARD"<<endl;
-   for ( int row=0; row<board.size(); row++)
-   {
-     cout<< "Player "<<row+1<< ": ";
-     for( int col=0; col<board[row].size(); col++)
-     {
-       board[row][col]=0;
-       cout<<board[row][col]<< "|";
-     }
-     cout<<endl;
-   }
+    //make scoreboard and fill it with zeros
+    board.resize(teams);
+    for (int row=0; row<board.size(); row++)
+    {
+      board[row].resize(periods);
+    }
+  }
 
   
-   //once created, display the scoreboard
+  //once created, display the scoreboard
+
+  printScoreboard(board);
+  for (int row=0; row<board.size(); row++)
+  {
+   for (int col=0; col<board[row].size(); col++)
+   {
+     board[row][col] = randomBetween(0,9);
+   }
+   cout<<endl;
   }
+   
   return 0;
+}
+
+void printScoreboard(vector< vector<int> > grid)
+{
+  cout<< "SCOREBOARD"<<endl;
+  for ( int row=0; row<grid.size(); row++)
+  {
+    cout<< "Player"<<row+1<< ": ";
+    for (int col=0; col<grid[row].size(); col++)
+    {
+      grid[row][col]=0;
+      cout<<grid[row][col]<< "|";
+    }
+    cout<<endl;
+  }
+}
+int randomBetween(int first, int second)
+{
+  if(first>second)
+  {
+    return second + rand()%(first-second+1);
+  }
+  else
+  {
+    return first + rand()%(second-first+1);
+  }
 }
